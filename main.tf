5e1af4d3d38512009e285eb4989c985e65aa04ef
Michal Majewski <michal_majewski@epam.com>
2023-03-06 13:14:28 +0100

    Reformat terraform files

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "4.52.0"
+      source  = "hashicorp/aws"
+      version = "4.52.0"
     }
   }
 }
